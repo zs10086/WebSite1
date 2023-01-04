b2ef47b916080615b798c0f9a4ac29f07aba5090
zs10086 <1074212278@qq.com>
2023-01-05 01:40:52 +0800

    添加WebApi

---
+++ WebSite1/App_Code/WebApiConfig.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web.Http
+
+Public Module WebApiConfig
+    Public Sub Register(ByVal config As HttpConfiguration)
+        ' Web API 配置和服务
+
+        ' Web API 路由
+        config.MapHttpAttributeRoutes()
+
+        config.Routes.MapHttpRoute(
+            name:="DefaultApi",
+            routeTemplate:="api/{controller}/{id}",
+            defaults:=New With {.id = RouteParameter.Optional}
+        )
+
+        config.Formatters.Remove(config.Formatters.XmlFormatter)
+    End Sub
+End Module
--- WebSite1/Global.asax
+++ WebSite1/Global.asax
@@ -1,10 +1,12 @@
 ﻿<%@ Application Language="VB" %>
 <%@ Import Namespace="System.Web.Optimization" %>
 <%@ Import Namespace="System.Web.Routing" %>
+<%@ Import Namespace="System.Web.Http" %>
 
 <script runat="server">
 
     Sub Application_Start(sender As Object, e As EventArgs)
+        GlobalConfiguration.Configure(AddressOf WebApiConfig.Register)
         RouteConfig.RegisterRoutes(RouteTable.Routes)
         BundleConfig.RegisterBundles(BundleTable.Bundles)
     End Sub
